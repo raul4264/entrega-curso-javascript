cd0a32bc33de8b643e61efaeeb1765c078f991d1
raulNovas <raulNovas4@gmail.com>
2021-05-13 20:55:07 -0300

    Entrega clase uno.

---
+++ archivo.js
@@ -0,0 +1,21 @@
+var edad = 57;
+
+var nacimiento = 1964;
+var actual = nacimiento + edad;
+document.write("El año en curso es.." + actual);
+document.write(" ");
+document.write("mi edad es de.." + edad + " " + "y mi año de nacimiento es.." + nacimiento);
+document.write(" ");
+var nombre = "Raul";
+var apellido = "Novas";
+
+document.write(" ");
+document.write("mi nombre completo es.." + nombre + " " + apellido)
+
+var pregunta = prompt("ingresar su nombre");
+var pregunta1 = prompt("ingresar su apellido");
+var edad = prompt("ingrese su edad");
+var edadCorregida = parseInt(edad)
+
+alert("mi nombre completo es.." + " " + pregunta + " " + pregunta1 + " y tengo" + " " + edad + " " + "años");
+ff
