0df234b48ab44bd71c0cf59fcda37e8263dc595e
raulNovas <raulNovas4@gmail.com>
2021-05-17 22:35:36 -0300

    Entrega clase dos. primera modificacion

--- archivo.js
+++ archivo.js
@@ -48,3 +48,41 @@ if (valorBoolean) {
 } else {
     console.log("falso")
 }
+var nombre = prompt("ingrese nombre");
+var apellido = prompt("ingrese apellido");
+if (nombre != "" && apellido != "") {
+    console.log(nombre + " es distinto de vacio y.." + apellido + " es distinto de vacio..");
+} else {
+    console.log("nombre o apellido vacios..");
+}
+var culpable = window.prompt("¿eres culpable?"); // si, no
+
+if (culpable == "si") {
+    document.write("irás a la cárcel");
+} else if (culpable == "no") {
+    document.write("irás a casa");
+} else {
+    document.write("la documentación por favor");
+}
+document.write("...");
+
+var topp = prompt("qué ingrediente quieres entre.. oreo,kitkat,brownie,lacasitos?..");
+var precio = 0.00;
+var helado = 1.90;
+var precioFinal = 0.00;
+
+if (topp == "oreo") {
+    precio = 1;
+} else if (topp == "kitkat") {
+    precio = 1.50;
+} else if (topp == "brownie") {
+    precio = 0.75;
+} else if (topp == "lacasitos") {
+    precio = 0.95;
+} else {
+    document.write("no tenemos este ingrediente, lo sentimos. ");
+    precio = 0;
+}
+
+precioFinal = helado + precio;
+document.write("el helado cuesta " + precioFinal + " $");
